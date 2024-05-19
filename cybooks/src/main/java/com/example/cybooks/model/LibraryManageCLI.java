0d863a64edb5d34d7be701019394549aa14ce627
TheSalhi94 <ilyass.salhi15@gmail.com>
2024-05-19 17:44:39 +0200

    changement de la classe LibraryManageCLI.java

--- cybooks/src/main/java/com/example/cybooks/model/LibraryManageCLI.java
+++ cybooks/src/main/java/com/example/cybooks/model/LibraryManageCLI.java
@@ -1,5 +1,9 @@
 package com.example.cybooks.model;
 
+
+import com.example.cybooks.exception.*;
+
+
 import java.util.Scanner;
 import java.time.LocalDate;
 
@@ -8,7 +12,7 @@ import java.time.LocalDate;
  * It represents the step before the development of a graphic interface using JavaFX.
  */
 
-public class LibraryManageCLI {
+public class LibraryManageCLI  {
     private DataBase db;
     private LibraryManager libraryManager;
 
@@ -39,7 +43,7 @@ public class LibraryManageCLI {
                     deleteUser(scanner);
                     break;
                 case 4 :
-                    lendBook(scanner);
+                    loanBook(scanner);
                     break;
                 case 5 :
                     returnBook(scanner);
@@ -51,7 +55,7 @@ public class LibraryManageCLI {
                     searchBook(scanner);
                     break;
                 case 8:
-                    printAllLoans(scanner);
+                    printAllLoans();
                     break;
                 case 9:
                     printOverdueLoans(scanner);
@@ -60,10 +64,10 @@ public class LibraryManageCLI {
                     printUserProfile(scanner);
                     break;
                 case 11:
-                    printAllBookLoaned(scanner);
+                    printAllBookLoaned();
                     break;
                 case 12:
-                    printMostLoanedBooksLast30d(scanner);
+                    printMostLoanedBooksLast30d();
                     break;
                 case 13:
                     exit = true;
@@ -92,90 +96,216 @@ public class LibraryManageCLI {
         System.out.println("13. Exit.");
     }
 
-    private void registerUser(Scanner scanner){
-        System.out.println("Enter the user's name :");
+    private void registerUser(Scanner scanner) { //done
+        System.out.print("Enter name: ");
         String name = scanner.nextLine();
-        System.out.println("Enter the user's email :");
+        System.out.print("Enter email: ");
         String email = scanner.nextLine();
-        System.out.println("Enter the user's address :");
+        System.out.print("Enter address: ");
         String address = scanner.nextLine();
 
-        try{
-            User user = new User(name, email, address);
+        try {
             libraryManager.registerUser(name, email, address);
-            System.out.println("User registered with ID : " + user.getUserID());
-        } catch (IllegalArgumentException e){
-            System.out.println("Error :" + e.getMessage());
-        
+            System.out.println("User registered successfully.");
+        } catch (InvalidEmailFormatException | EmailAlreadyExistsException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
-    private void updateUser(Scanner scanner){
-        System.out.println("Enter the user's ID :");
+    private void updateUser(Scanner scanner){ //done
+        System.out.print("Enter user ID: ");
         int userID = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter the user's new name :");
+        scanner.nextLine();  // Consume newline
+
+        if (!libraryManager.userExists(userID)) {
+            System.out.println("User not found.");
+            return;
+        }
+
+        System.out.print("Enter new name (leave empty to keep current): ");
         String name = scanner.nextLine();
-        System.out.println("Enter the user's new email :");
+
+        System.out.print("Enter new email (leave empty to keep current): ");
         String email = scanner.nextLine();
-        System.out.println("Enter the user's new address :");
+
+        System.out.print("Enter new address (leave empty to keep current): ");
         String address = scanner.nextLine();
 
-        try{
-            User user = new User(userID, name, email, address);
+        try {
             libraryManager.updateUser(userID, name, email, address);
-            System.out.println("User updated with ID : " + user.getUserID());
-        } catch (IllegalArgumentException e){
-            System.out.println("Error :" + e.getMessage());
+            System.out.println("User updated successfully.");
+        } catch (UserNotFoundException | InvalidEmailFormatException | EmailAlreadyExistsException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
-    private void deleteUser(Scanner scanner){
-        System.out.println("Enter the user's ID :");
+    private void deleteUser(Scanner scanner){ // done + we need to add loan verification
+        System.out.print("Enter user ID: ");
         int userID = scanner.nextInt();
-        scanner.nextLine();
+        scanner.nextLine();  // Consume newline
 
-        try{
+        try {
             libraryManager.deleteUser(userID);
-            System.out.println("User deleted with ID : " + userID);
-        } catch (IllegalArgumentException e){
-            System.out.println("Error :" + e.getMessage());
+            System.out.println("User deleted successfully.");
+        } catch (UserNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void loanBook(Scanner scanner){ //Done
+        System.out.print("Enter user ID: ");
+        int userID = scanner.nextInt();
+        scanner.nextLine();  // Consume newline
+        if (!libraryManager.userExists(userID)) {
+            System.out.println("User not found.");
+            return;
+        }
+        System.out.print("Enter ISBN: ");
+        String isbn = scanner.nextLine();
+
+        if (!libraryManager.isbnExistsInBNF(isbn)) {
+            System.out.println("Book not found.");
+            return;
+        }
+
+        try {
+            libraryManager.loanBook(userID, isbn);
+            System.out.println("Loan added successfully.");
+        } catch (UserNotFoundException | NoCopyAvailableException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void returnBook(Scanner scanner){ //Done
+        System.out.print("Enter user ID: ");
+        int userID = scanner.nextInt();
+        scanner.nextLine();  // Consume newline
+
+        if (!libraryManager.userExists(userID)) {
+            System.out.println("User not found.");
+            return;
         }
         
-    }
+        System.out.print("Enter ISBN of the loaned book :");
+        String isbn = scanner.nextLine();
 
-    private void lendBook(Scanner scanner){
-        System.out.println("Enter the user's ID :");
-        int userID = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter the book's ISBN :");
-        String bookISBN = scanner.nextLine();
-        scanner.nextLine();
-
-        try{
-            libraryManager.loanBook(userID, bookISBN);
-            System.out.println("Book lent to user with ID : " + userID);
-        } catch (IllegalArgumentException e){
-            System.out.println("Error :" + e.getMessage());
+        try {
+            libraryManager.returnBook(userID, isbn);
+            System.out.println("Book returned successfully.");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
-    private void returnBook(Scanner scanner){
-        System.out.println("Enter the user's ID :");
-        int userID = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter the book's ID :");
-        int bookID = scanner.nextInt();
+    private void searchUser(Scanner scanner) {
+        System.out.print("How do you want to search for the user ?\n1. By ID\n2. By email\nEnter your choice :");
+        int choice = scanner.nextInt();
         scanner.nextLine();
 
-        try{
-            //Get the right loanID :
-            int loanID = libraryManager.getLoanIDbyUserAndBookID(userID, bookID);
-            libraryManager.returnBook(loanID);
-            System.out.println("Book returned by user with ID : " + userID);
-        } catch (IllegalArgumentException e){
-            System.out.println("Error :" + e.getMessage());
+        switch (choice) {
+            case 1:
+                searchUserByID(scanner);
+                break;
+            case 2:
+                searchUserByEmail(scanner);
+                break;
+            default:
+                System.out.println("Invalid choice, please try again.");
         }
+    }
+
+    private void searchUserByID(Scanner scanner) { //Done
+        System.out.print("Enter user ID: ");
+        int userID = scanner.nextInt();
+        scanner.nextLine();  // Consume newline
+
+        try {
+            User user = libraryManager.searchUser(userID);
+            System.out.println(user.toString());
+        } catch (UserNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void searchUserByEmail(Scanner scanner) { //Done
+        System.out.print("Enter user email: ");
+        String email = scanner.nextLine();
+
+        try {
+            User user = libraryManager.searchUser(email);
+            System.out.println(user.toString());
+        } catch (UserNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void searchBook(Scanner scanner) { //Pending BNF API methods
+        System.out.print("Enter ISBN: ");
+        String isbn = scanner.nextLine();
+
+        try {
+            String result = libraryManager.searchBook(isbn);
+            System.out.println(result);
+        } catch (BookNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void printAllLoans() { //Done
+        String result = libraryManager.viewLoans(false, false);
+        System.out.println(result);
+    }
+
+    private void printOverdueLoans(Scanner scanner) { //Done
+        System.out.print("Today's date is : ");
+        System.out.println(LocalDate.now());
+
+        try {
+            String result = libraryManager.viewLoans(true, true);
+            System.out.println(result);
+            System.out.println("End of overdue loans.");
+        } catch (Exception e) {
+            System.out.println("Error: Invalid date format.");
+        }
+    }
+
+    private void printUserProfile(Scanner scanner) {
+        System.out.print("Enter user ID: ");
+        int userID = scanner.nextInt();
+        scanner.nextLine();  // Consume newline
+
+        try {
+            User user = libraryManager.searchUser(userID);
+            String userLoans = libraryManager.getUserLoans(userID);
+
+            System.out.println("\nHere are the user's informations :");
+            System.out.println(user.toString() + "\n");
+            System.out.println("Here are the user's loans :");
+            System.out.println(userLoans);
+
+        } catch (UserNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void printAllBookLoaned() {
+
+        System.out.println("Here are all the books currently loaned :");
+        String result = libraryManager.viewLoans(true, false);
+        System.out.println(result);
+    }
+
+    private void printMostLoanedBooksLast30d(){
+        System.out.println("Here are the 5 most loaned books in the last 30 days :");
+        String result = libraryManager.mostLoanedBooksLast30d();
+        System.out.println(result);
+    }
+
+
+    // Main method to run the CLI
+    public static void main(String[] args) {
+        LibraryManageCLI cli = new LibraryManageCLI();
+        cli.run();
     }
 
 }
