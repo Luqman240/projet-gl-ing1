d26a423eeec8e7bf053756e4fa56afd6125c827d
TheSalhi94 <ilyass.salhi15@gmail.com>
2024-05-19 18:05:43 +0200

    grand changement

--- cybooks/src/main/java/com/example/cybooks/model/BNFApiConnector.java
+++ cybooks/src/main/java/com/example/cybooks/model/BNFApiConnector.java
@@ -1,23 +1,5 @@
 package com.example.cybooks.model;
 
 public class BNFApiConnector {
-
-    private String apiURL;
-
-    public BNFApiConnector(String apiURL) {
-        this.apiURL = apiURL;
-    }
-
-    public String getApiURL() {
-        return apiURL;
-    }
-
-    public void setApiURL(String apiURL) {
-        this.apiURL = apiURL;
-    }
-
-    public fetchBookDetails(String isbn) {
-        // Code to fetch book details from the BNF API
-        return "Book details for ISBN " + isbn;
-    }
+    
 }
