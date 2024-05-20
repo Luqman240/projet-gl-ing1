9c5eaedfc4fb9d1285d646dc40f33a0571758a04
Youns777 <benabdella@cy-tech.fr>
2024-05-20 21:20:05 +0200

    Suppression fichiers

--- cybooks/src/main/java/com/example/cybooks/exception/InvalidEmailFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cybooks.exception;
-
-public class InvalidEmailFormatException extends Exception {
-    public InvalidEmailFormatException(String message) {
-        super(message);
-    }
-}
--- cybooks/src/main/java/com/example/cybooks/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cybooks.exception;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
