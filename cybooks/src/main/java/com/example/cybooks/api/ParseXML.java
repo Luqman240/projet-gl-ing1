1fd80be8e8574dfb27df7f6accfc8a9fc3c9b267
TheSalhi94 <ilyass.salhi15@gmail.com>
2024-05-21 19:40:21 +0200

    suppression de methode non utilise

--- cybooks/src/main/java/com/example/cybooks/api/ParseXML.java
+++ cybooks/src/main/java/com/example/cybooks/api/ParseXML.java
@@ -7,8 +7,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import com.example.cybooks.model.BookApi;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 import java.io.ByteArrayInputStream;
