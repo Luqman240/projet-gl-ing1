56a8146d0c24ef926e1e1905f1d2fb309ac813ee
TheSalhi94 <157057719+TheSalhi94@users.noreply.github.com>
2024-05-23 20:43:06 +0200

    Delete cybooks directory

--- cybooks/src/main/java/com/example/cybooks/api/ApiConnector.java
+++
@@ -1,4 +0,0 @@
-package com.example.cybooks.api;
-
-public class ApiConnector {
-}
--- cybooks/src/main/java/com/example/cybooks/exception/UserNotIntException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cybooks.exception;
-
-public class UserNotIntException extends Exception {
-    public UserNotIntException(String message) {
-        super(message);
-    }
-}
